f1992b907353297941378f09dc042c6c1ec2c3f2
SCN3 <scn3@zju.edu.cn>
2015-12-23 23:50:27 +0800

    SerenityOS: update cmd.mips, editor.mips and runcmd.bat

--- SerenityOS/runcmd.bat
+++ SerenityOS/runcmd.bat
@@ -1,5 +1,7 @@
 SerenityASM.exe src\cmd.mips
-move src\cmd.bit H:\SHELL.BIT
+move src\cmd.bit F:\SHELL.BIT \y
+SerenityASM.exe src\editor.mips
+move src\editor.bit F:\EDIT.BIT \y
 cd ..
 mingw32-make 
 cd SerenityOS
